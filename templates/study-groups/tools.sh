ccee3c34aa7f9751cb8ff4171302a2a806d32679
Mike Ricos <mike.ricos@gmail.com>
2021-02-21 04:02:44 -0600

    Preparing for build.

--- templates/study-groups/tools.sh
+++ templates/study-groups/tools.sh
@@ -5,17 +5,26 @@ tools-build(){
   rsync ./index.html $BUILD/$VER/
   rsync ./styleguide.html $BUILD/$VER/
   rsync ./error.html $BUILD/$VER/
-  ln -f -s $BUILD/assets $BUILD/$VER/assets
+  cp -r ./assets $BUILD/$VER/assets
+  #ln -f -s $BUILD/assets $BUILD/$VER/assets
 }
 
 tools-list-build(){
   ls -l $BUILD/$VER 
 }
 tools-publish(){
+  echo: git add $BUILD/$VER && git commit -m "Initial subtree commit."
+  echo: git subtree push --prefix $BUILD/$VER origin gh-pages  
   git add $BUILD/$VER && git commit -m "Initial subtree commit."
   git subtree push --prefix $BUILD/$VER origin gh-pages  
 }
 
 tools-clean(){
-  echo rm -r $BUILD/$VER
+  echo executing: rm -r $BUILD/$VER
+  rm -r $BUILD/$VER
+}
+
+tools-show-nginx(){
+
+  cat /etc/nginx/sites-enabled/study-groups.org
 }
