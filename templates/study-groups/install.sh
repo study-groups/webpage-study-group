579072ff63dfbd41637041e457638ce33c9244c7
Mike Ricos <mike.ricos@gmail.com>
2021-02-21 03:04:58 -0600

    Added error page example to styleguide.

--- templates/study-groups/install.sh
+++ templates/study-groups/install.sh
@@ -1,3 +1,12 @@
+VER=001pre4
+BUILD=$(realpath ../../builds/study-groups)
+
 install-webpage(){
-  git subtree push --prefix build/001 origin gh-pages  
+  rsync ./index.html $BUILD/$VER/
+  rsync ./styleguide.html $BUILD/$VER/
+  rsync ./error.html $BUILD/$VER/
+  ln -f -s $BUILD/assets $BUILD/$VER/assets
+  #rsync ./assets $BUILD/
+  
+  #git subtree push --prefix build/001 origin gh-pages  
 }
