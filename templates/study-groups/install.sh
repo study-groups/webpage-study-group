bf81c81da6f69066157b6819e66e6381917c2925
Mike Ricos <mike.ricos@gmail.com>
2021-02-21 03:08:37 -0600

    Adds clean build function.

--- templates/study-groups/install.sh
+++ templates/study-groups/install.sh
@@ -1,12 +1,14 @@
 VER=001pre4
 BUILD=$(realpath ../../builds/study-groups)
 
-install-webpage(){
+webpage-install(){
   rsync ./index.html $BUILD/$VER/
   rsync ./styleguide.html $BUILD/$VER/
   rsync ./error.html $BUILD/$VER/
   ln -f -s $BUILD/assets $BUILD/$VER/assets
-  #rsync ./assets $BUILD/
-  
   #git subtree push --prefix build/001 origin gh-pages  
+}
+
+webpage-remove(){
+  echo rm -r $BUILD/$VER
 }
